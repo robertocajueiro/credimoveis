dc83c924ebac0c83e7773692c58c2220dd29639e
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-12-14 17:54:39 -0300

    SideBar

--- credimoveis-app/src/app/app.module.ts
+++ credimoveis-app/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { TemplateModule } from './template/template.module'
+import { TemplateModule } from './template/template.module';
 
 @NgModule({
   declarations: [
