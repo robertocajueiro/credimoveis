965a0243df6ebab307a49d961285f22d870309a0
Roberto Cajueiro <beto.cajueiro@gmail.com>
2021-12-20 15:19:19 -0300

    correção Sidbar

--- credimoveis-app/src/app/app.component.ts
+++ credimoveis-app/src/app/app.component.ts
@@ -7,8 +7,28 @@ import jQuery from 'jquery'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'credimoveis-app';
+  href: string | undefined;
 
+  ngAfterViewInit() {
+      (($) => {
+        "use strict";
+
+        // Add active state to sidbar nav links
+        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(() => {
+                if (this.href === path) {
+                    $(this).addClass("active");
+                }
+            });
+
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function(e: { preventDefault: () => void; }) {
+            e.preventDefault();
+            $("body").toggleClass("sb-sidenav-toggled");
+        });
+    })(jQuery);
+  }
 
 }
